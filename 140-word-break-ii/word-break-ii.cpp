33f88a9c46fdae228f0dab4d0540ad18ef4a27ee
Rubesh Raj R <rrrkabil15@gmail.com>
2026-08-11 19:18:19 +0530

    Time: 2 ms (19.39%) | Memory: 11 MB (20.08%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    vector<string> solve(string s,int index,unordered_set<string>&st){
+    vector<string> solve(string s,int index,unordered_set<string>&st,vector<vector<string>>&dp,vector<bool>&visited){
         vector<string>ans;
         if(index==s.size()){
-            ans.push_back("");
-            return ans;
+            return {""};
+        }
+        if(visited[index]){
+            return dp[index];
         }
         for(int end=index;end<s.size();end++){
             string word=s.substr(index,end-index+1);
             if(st.find(word)!=st.end()){
-                vector<string>remaining=solve(s,end+1,st);
+                vector<string>remaining=solve(s,end+1,st,dp,visited);
                 for(auto m:remaining){
                     if(m==""){
                         ans.push_back(word);
@@ -20,6 +22,7 @@ public:
                 }
             }
         }
+        dp[index]=ans;
         return ans;
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
@@ -27,6 +30,9 @@ public:
         for(string c:wordDict){
             st.insert(c);
         }
-        return solve(s,0,st);
+        int n=s.size();
+        vector<vector<string>>dp(n+1);
+        vector<bool>visited(n+1,false);
+        return solve(s,0,st,dp,visited);
     }
 };

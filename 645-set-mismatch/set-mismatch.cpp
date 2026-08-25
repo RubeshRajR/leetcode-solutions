b4a112caa28e68d000700a446c8636d5bb9a7858
Rubesh Raj R <rrrkabil15@gmail.com>
2026-08-25 13:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (98.14%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i:nums){
-            mp[i]++;
-        }
-         int duplicate = -1;
+        int duplicate = -1;
         int missing = -1;
-        for (int i = 1; i <= nums.size(); i++) {
-            if (mp[i] == 2) {
-                duplicate=i;     
-            }
-            else if (mp[i] == 0) {
-               missing=i;      
+
+        for (int x : nums) {
+            int index = abs(x) - 1;
+
+            if (nums[index] < 0)
+                duplicate = abs(x);
+            else
+                nums[index] *= -1;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
             }
         }
-        return {duplicate,missing};
+
+        return {duplicate, missing};
     }
 };

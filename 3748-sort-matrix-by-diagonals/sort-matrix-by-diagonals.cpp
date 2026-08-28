62a193d0d5db80f17a20522db5d88fc6d63919a8
Rubesh Raj R <rrrkabil15@gmail.com>
2026-08-28 21:29:15 +0530

    Time: 21 ms (6.92%) | Memory: 47.3 MB (6.38%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int r=0;r<m;r++){
-            vector<int>v;
-            int i=r;
-            int j=0;
-            while(i<m&&j<n){
-                v.push_back(grid[i][j]);
-                i++;
-                j++;
-            }
-            sort(v.rbegin(),v.rend());
-            i=r;
-            j=0;
-            int k=0;
-            while(i<m&&j<n){
-                grid[i][j]=v[k++];
-                i++;
-                j++;
+        int r=grid.size();
+        int c=grid[0].size();
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                mp[i-j].push_back(grid[i][j]);
             }
         }
-        for(int c=1;c<n;c++){
-            vector<int>v;
-            int j=c;
-            int i=0;
-            while(i<m&&j<n){
-                v.push_back(grid[i][j]);
-                i++;
-                j++;
+        for(auto &p:mp){
+            if(p.first>=0){
+                sort(p.second.rbegin(),p.second.rend());
             }
-            int k=0;
-            i=0;
-            j=c;
-            sort(v.begin(),v.end());
-            while(i<m&&j<n){
-                grid[i][j]=v[k++];
-                i++;
-                j++;
+            else{
+                sort(p.second.begin(),p.second.end());
+            }
+        }
+        map<int,int>index;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                int key=i-j;
+                grid[i][j]=mp[key][index[key]];
+                index[key]++;
             }
         }
         return grid;

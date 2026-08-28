0f837e7ae1d0ea34d8d08ea57765c01a40f1b300
Rubesh Raj R <rrrkabil15@gmail.com>
2026-08-28 19:32:39 +0530

    Time: 3 ms (53.24%) | Memory: 13.5 MB (89.48%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,24 +1,70 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int r=mat.size();
-        int c=mat[0].size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                mp[i-j].push_back(mat[i][j]);
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        // Start from every row in first column
+        for(int r = 0; r < m; r++)
+        {
+            vector<int> v;
+
+            int i = r;
+            int j = 0;
+
+            while(i < m && j < n)
+            {
+                v.push_back(mat[i][j]);
+                i++;
+                j++;
+            }
+
+            sort(v.begin(), v.end());
+
+            i = r;
+            j = 0;
+
+            int k = 0;
+
+            while(i < m && j < n)
+            {
+                mat[i][j] = v[k++];
+                i++;
+                j++;
             }
         }
-        for(auto &p:mp){
-            sort(p.second.rbegin(),p.second.rend());
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                int key=i-j;
-                mat[i][j]=mp[key].back();
-                mp[key].pop_back();
+
+        // Start from every column in first row except (0,0)
+        for(int c = 1; c < n; c++)
+        {
+            vector<int> v;
+
+            int i = 0;
+            int j = c;
+
+            while(i < m && j < n)
+            {
+                v.push_back(mat[i][j]);
+                i++;
+                j++;
+            }
+
+            sort(v.begin(), v.end());
+
+            i = 0;
+            j = c;
+
+            int k = 0;
+
+            while(i < m && j < n)
+            {
+                mat[i][j] = v[k++];
+                i++;
+                j++;
             }
         }
+
         return mat;
     }
 };

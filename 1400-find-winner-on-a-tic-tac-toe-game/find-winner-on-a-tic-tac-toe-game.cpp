f45a11a3bfc563e1d39b760316dca8081c79645e
Rubesh Raj R <rrrkabil15@gmail.com>
2026-09-04 18:14:27 +0530

    Time: 3 ms (5.28%) | Memory: 10.7 MB (54.81%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.cpp
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>&board,char p){
-        for(int i=0;i<3;i++){
-            if(board[i][0]==p&&board[i][1]==p&&board[i][2]==p){
-                return true;
-            }
-            if(board[0][i]==p&&board[1][i]==p&&board[2][i]==p){
-                return true;
-            }
-        }
-        if(board[0][0]==p&&board[1][1]==p&&board[2][2]==p){
-            return true;
-        }
-        if(board[0][2]==p&&board[1][1]==p&&board[2][0]==p){
-            return true;
-        }
-        return false;
-    }
-
     string tictactoe(vector<vector<int>>& moves) {
-        vector<vector<char>>board(3,vector<char>(3,'.'));
-        for(int i=0;i<moves.size();i++){
-            int r=moves[i][0];
-            int c=moves[i][1];
-            char player;
-            if(i%2==0){
-                player='A';
+        vector<int> row(3, 0);
+        vector<int> col(3, 0);
+        int diagonal = 0;
+        int secdiagonal = 0;
+
+        for (int i = 0; i < moves.size(); i++) {
+            int r = moves[i][0];
+            int c = moves[i][1];
+
+            // A = +1, B = -1
+            int value = (i % 2 == 0) ? 1 : -1;
+
+            row[r] += value;
+            col[c] += value;
+
+            if (r == c) {
+                diagonal += value;
             }
-            else{
-                player='B';
+
+            if (r + c == 2) {
+                secdiagonal += value;
             }
-            board[r][c]=player;
-            if(solve(board,player)){
-                return string(1,player);
+
+            if (abs(row[r]) == 3 ||
+                abs(col[c]) == 3 ||
+                abs(diagonal) == 3 ||
+                abs(secdiagonal) == 3) {
+                return (value == 1) ? "A" : "B";
             }
         }
-        if(moves.size()==9){
+
+        if (moves.size() == 9) {
             return "Draw";
         }
+
         return "Pending";
     }
 };

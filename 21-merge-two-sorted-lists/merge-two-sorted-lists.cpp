cccee5634600fc74d0e3fe058b6c2f48ff0430f4
Rubesh Raj R <rrrkabil15@gmail.com>
2026-09-09 23:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (27.43%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,43 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        vector<int> arr;
-
-        while(list1 != NULL) {
-            arr.push_back(list1->val);
-            list1 = list1->next;
+        ListNode*dummy=new ListNode(0);
+        ListNode*tail=dummy;
+        while(list1!=NULL&&list2!=NULL){
+            if(list1->val<list2->val){
+                tail->next=list1;
+                list1=list1->next;
+            }
+            else{
+                tail->next=list2;
+                list2=list2->next;
+            }
+            tail=tail->next;
         }
-
-        while(list2 != NULL) {
-            arr.push_back(list2->val);
-            list2 = list2->next;
+        if(list1!=NULL){
+            tail->next=list1;
         }
-
-        sort(arr.begin(), arr.end());
-
-        if(arr.empty())
-            return NULL;
-
-        // ListNode* head = new ListNode(arr[0]);
-        // ListNode* tail = head;
-
-        // for(int i = 1; i < arr.size(); i++) {
-
-        //     tail->next = new ListNode(arr[i]);
-
-        //     tail = tail->next;
-        // }
-
-        // return head;
-          ListNode* dummy = new ListNode(0);
-        ListNode* tail = dummy;
-
-        for(int x : arr) {
-            tail->next = new ListNode(x);
-            tail = tail->next;
+        else{
+            tail->next=list2;
         }
-
         return dummy->next;
     }
 };

eb595b4fad91b765efc1976b713dbf87445dc97c
Rubesh Raj R <rrrkabil15@gmail.com>
2026-09-09 22:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (74.42%) - LeetSync

--- 4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.cpp
+++ 4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.cpp
@@ -1,37 +1,94 @@
 class Solution {
 public:
     int maxDigitRange(vector<int>& nums) {
-        int ans = 0;
-        vector<pair<int, int>> p;
-        int maxRange = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            int temp = nums[i];
+        int maxRange = -1;
+        int ans = 0;
+
+        for (int num : nums) {
+
+            int temp = num;
+
             int mini = INT_MAX;
             int maxi = INT_MIN;
+
+            // Special case for number 0
             if (temp == 0) {
                 mini = maxi = 0;
             }
+
+            // Find minimum and maximum digit
             while (temp > 0) {
+
                 int digit = temp % 10;
 
-                if (digit > maxi)
-                    maxi = digit;
+                mini = min(mini, digit);
+                maxi = max(maxi, digit);
 
-                if (digit < mini)
-                    mini = digit;
-                temp /= 10; 
+                // VERY IMPORTANT:
+                // Remove the last digit
+                temp /= 10;
             }
+
+            // Example:
+            // num = 456
+            // mini = 4, maxi = 6
+            // range = 6 - 4 = 2
+
             int range = maxi - mini;
-            p.push_back({nums[i], range});
+
+
+            // CASE 1:
+            // We found a NEW maximum range.
             if (range > maxRange) {
+
                 maxRange = range;
+
+                // Previous numbers had smaller range,
+                // so they are no longer useful.
+                //
+                // Therefore, RESET ans to current number.
+                ans = num;
             }
-        }
-        for (auto a : p) {
-            if (a.second == maxRange) {
-                ans += a.first;
+
+
+            // CASE 2:
+            // Current number has the SAME maximum range.
+            else if (range == maxRange) {
+
+                // Current number also belongs to the answer.
+                // So ADD it to ans.
+                ans += num;
             }
+
+            /*
+                Example:
+
+                nums = {123, 456, 111, 909}
+
+                123 -> range = 2
+                    maxRange = 2
+                    ans = 123
+
+                456 -> range = 2
+                    same maximum
+                    ans = 123 + 456
+                        = 579
+
+                111 -> range = 0
+                    smaller than maxRange
+                    ans remains 579
+
+                909 -> range = 9
+                    NEW maximum!
+                    Previous ans (579) is useless now.
+                    So RESET:
+                    ans = 909
+
+                Final:
+                maxRange = 9
+                ans = 909
+            */
         }
 
         return ans;

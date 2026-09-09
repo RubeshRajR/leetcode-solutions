9bd0d4dbf0abcfd41a77ddf91a62a7c7ec2e19be
Rubesh Raj R <rrrkabil15@gmail.com>
2026-09-09 22:44:14 +0530

    Time: 2 ms (4.14%) | Memory: 19.9 MB (8.33%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        vector<int> arr;
+
+        while(list1 != NULL) {
+            arr.push_back(list1->val);
+            list1 = list1->next;
+        }
+
+        while(list2 != NULL) {
+            arr.push_back(list2->val);
+            list2 = list2->next;
+        }
+
+        sort(arr.begin(), arr.end());
+
+        if(arr.empty())
+            return NULL;
+
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* tail = head;
+
+        for(int i = 1; i < arr.size(); i++) {
+
+            tail->next = new ListNode(arr[i]);
+
+            tail = tail->next;
+        }
+
+        return head;
+    }
+};

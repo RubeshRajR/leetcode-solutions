3c3441ef5b116704747991a81512abebf2098890
Rubesh Raj R <rrrkabil15@gmail.com>
2026-09-16 19:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (63.87%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -3,9 +3,9 @@ public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int row=matrix.size();
         int col=matrix[0].size();
-        for(int i=0;i<row-1;i++){
-            for(int j=0;j<col-1;j++){
-                if(matrix[i][j]!=matrix[i+1][j+1]){
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
                     return false;
                 }
             }
